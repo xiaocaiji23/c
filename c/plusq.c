b59b175d469f0a63a720f19f532b5efea0ec632a
lidongzhi <3221680836@qq.com>
2023-09-23 13:19:33 +0800

    modified:   c/plusq.c 	modified:   c/plusq.exe 	modified:   c/plusq.c 	modified:   c/plusq.exe 	deleted:    plusq.c 	.vscode/ 	c/alphabet.c 	c/alphabet.exe 	c/checking.c 	c/checking.exe 	c/menuette.c 	c/misuse.c 	c/misuse.exe

--- c/plusq.c
+++ c/plusq.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 int main(void)
 {
-    int a = 1;
+    /*int a = 1;
     int b = 2;
     //scanf("%d%d", &a, &b);
-    printf("%d", a=3);
+    printf("%d", a=3);*/
+    int a = 1;
+    int b = 2;
+    if (a<b||b++<a+b)
+    {
+        printf("b=%d,如果b的值是2||就符合&&的套路\n",b);
+    }
+    
     return 0;
 }
